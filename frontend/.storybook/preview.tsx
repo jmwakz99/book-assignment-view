85a39d42f939f57078a072f4ac34afdf78db6f1b
mwanjilajohnson <mwanjilajohnson@gmail.com>
2024-06-12 16:26:59 +0300

    chore: upgrade storybook

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -11,6 +11,7 @@ const preview: Preview = {
       </ThemeWrapper>
     ),
   ],
+
   parameters: {
     controls: {
       matchers: {
@@ -19,6 +20,8 @@ const preview: Preview = {
       },
     },
   },
+
+  tags: ["autodocs"]
 };
 
 export default preview;
